53f43e6670c0b718f9fffd42061bdbd1c657d102
Mikita Yurtsevich <nuktinov@gmail.com>
2023-06-09 04:07:25 +0200

    Initialize app with IdentityServer4 quick start

--- EventOrganizer.Identity/Program.cs
+++ EventOrganizer.Identity/Program.cs
@@ -1,25 +1,57 @@
+using EventOrganizer.Identity.IdentityConfiguration;
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var origins = new[]
+{
+    builder.Configuration.GetValue<string>("AllowedOrigins:WebClient"),
+    builder.Configuration.GetValue<string>("AllowedOrigins:WebApi")
+};
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityServer()
+    .AddInMemoryIdentityResources(IdentityConfig.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
+    .AddInMemoryClients(IdentityConfig.GetClients(origins.First()))
+    .AddInMemoryApiResources(IdentityConfig.ApiResources)
+    .AddTestUsers(TestUsers.Users)
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All ^ HttpLoggingFields.RequestHeaders;
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins(origins)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
 
-app.MapControllers();
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+
+app.UseHttpLogging();
+
+app.UseCors();
 
 app.Run();
