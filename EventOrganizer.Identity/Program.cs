8e78b21f65b23ee36303c994bc09d215102bfea8
Mikita Y <mikita-y@users.noreply.github.com>
2024-02-03 10:44:34 +0100

    MM-53: Create Utils nuget package for shared classes

--- EventOrganizer.Identity/Program.cs
+++ EventOrganizer.Identity/Program.cs
@@ -1,35 +1,12 @@
 using EventOrganizer.Identity.IdentityConfiguration;
+using EventOrganizer.Utils.WebApplicationExtensions;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.AspNetCore.HttpOverrides;
-using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
-if (builder.Configuration.GetValue<bool>("UseCustomSslCertificates"))
-{
-    var certs = new Dictionary<string, X509Certificate2>(StringComparer.OrdinalIgnoreCase)
-    {
-        ["localhost"] = new X509Certificate2("/app/certificates/localhost.pfx", "password"),
-        ["host.docker.internal"] = new X509Certificate2("/app/certificates/host.docker.internal.pfx", "password")
-    };
-
-    using (var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser))
-    {
-        store.Open(OpenFlags.ReadWrite);
-
-        store.Add(certs["localhost"]);
-        store.Add(certs["host.docker.internal"]);
-    }
-
-    builder.WebHost.ConfigureKestrel(options =>
-        options.ConfigureHttpsDefaults(opt =>
-        {
-            opt.SslProtocols = System.Security.Authentication.SslProtocols.Tls12;
-            opt.ServerCertificateSelector = (connectionContext, name) =>
-                name == "host.docker.internal" ? certs["host.docker.internal"] : certs["localhost"]; ;
-        }));
-}
+builder.ConfigureCertificates();
 
 var origins = new[]
 {
